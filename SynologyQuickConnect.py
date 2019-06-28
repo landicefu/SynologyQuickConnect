dd57bb522d4d96fded96543ef338d5f04dfd48ee
Landice Fu <landicefu@gmail.com>
2019-06-29 00:13:29 +0800

    Add ping method

--- SynologyQuickConnect.py
+++ SynologyQuickConnect.py
@@ -1,6 +1,12 @@
 import requests
 
 
+class Host:
+    def __init__(self, address: str, port: int):
+        self.address = address
+        self.port = port
+
+
 def get_server_info(quick_connect_id, _id, url='http://global.quickconnect.to/Serv.php'):
     payload = {
         'version': 1,
@@ -17,7 +23,18 @@ def get_server_info(quick_connect_id, _id, url='http://global.quickconnect.to/Se
         return get_server_info(quick_connect_id, _id, 'http://%s/Serv.php' % result.sites[0])
 
 
-def resolve_by_ping_dsm(server_info):
+def ping(host: Host, is_https: bool):
+    scheme = "https" if is_https else "http"
+    url = "%s://%s:%d/webman/pingpong.cgi?action=cors" % (scheme, host.address, host.port)
+    try:
+        result = requests.get(url, verify=False)
+        if result.status_code == 200 and result.json()['success']:
+            return host
+    except: pass
+    return None
+
+
+def resolve_by_ping_dsm(server_info, is_https: bool):
     server = server_info['server']
     service = server_info['service']
     port = service.port
@@ -25,27 +42,27 @@ def resolve_by_ping_dsm(server_info):
     return None
 
 
-def resolve_by_ping_tunnel(service):
+def resolve_by_ping_tunnel(service, is_https: bool):
     return None
 
 
-def request_tunnel(server_info, quick_connect_id, _id):
+def request_tunnel(server_info, quick_connect_id, _id, is_https: bool):
     return None
 
 
-def resolve(quick_connect_id, is_https=True):
+def resolve(quick_connect_id, is_https: bool=True):
     if ':' in quick_connect_id or '.' in quick_connect_id or quick_connect_id == '':
         raise Exception('%s is not a QuickConnect ID' % quick_connect_id)
 
     _id = 'dsm_portal_https' if is_https else 'dsm_portal'
     server_info = get_server_info(quick_connect_id, _id)
-    result = resolve_by_ping_dsm(server_info)
+    result = resolve_by_ping_dsm(server_info, is_https)
     if result is not None:
         return result
-    result = resolve_by_ping_tunnel(server_info['service'])
+    result = resolve_by_ping_tunnel(server_info['service'], is_https)
     if result is not None:
         return result
-    result = request_tunnel(server_info, quick_connect_id, _id)
+    result = request_tunnel(server_info, quick_connect_id, _id, is_https)
     if result is not None:
         return result
     return None
@@ -53,4 +70,4 @@ def resolve(quick_connect_id, is_https=True):
 
 if __name__ == '__main__':
     # print(resolve('landicefu', True))
-    rx.of("1")
+    print(ping(Host("61.220.55.78", 31763), True))
